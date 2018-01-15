8e2fde49b83a2e224d50d7c318c0f2c79d00a2ea
Miroslav <miroslav.pashaliski@codeit.mk>
2018-01-15 22:00:05 +0100

    new modules added

---
+++ convertow/convertow-jpg-to-png/src/main/java/com/convertow/ConvertowJpgToPng.java
@@ -0,0 +1,12 @@
+package com.convertow;
+
+/**
+ * This class is optional and represents the configuration for the convertow-jpg-to-png module.
+ * By exposing simple getter/setter/adder methods, this bean can be configured via content2bean
+ * using the properties and node from <tt>config:/modules/convertow-jpg-to-png</tt>.
+ * If you don't need this, simply remove the reference to this class in the module descriptor xml.
+ */
+public class ConvertowJpgToPng {
+    /* you can optionally implement info.magnolia.module.ModuleLifecycle */
+
+}
