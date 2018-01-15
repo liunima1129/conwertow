ba0b129eecf7e9d26dc5af5707d8a35885ef0d2b
Miroslav <miroslav.pashaliski@codeit.mk>
2018-01-15 21:56:32 +0100

    convertow pdf to png

---
+++ convertow/convertow-pdf-to-png/src/main/java/com/convertow/ConvertowPdfToPng.java
@@ -0,0 +1,12 @@
+package com.convertow;
+
+/**
+ * This class is optional and represents the configuration for the convertow-pdf-to-png module.
+ * By exposing simple getter/setter/adder methods, this bean can be configured via content2bean
+ * using the properties and node from <tt>config:/modules/convertow-pdf-to-png</tt>.
+ * If you don't need this, simply remove the reference to this class in the module descriptor xml.
+ */
+public class ConvertowPdfToPng {
+    /* you can optionally implement info.magnolia.module.ModuleLifecycle */
+
+}
