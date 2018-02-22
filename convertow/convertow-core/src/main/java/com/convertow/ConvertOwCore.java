201b530455fd12c9f2e0aef699e08d8ca0e4f7f0
Miroslav <miroslav.pashaliski@codeit.mk>
2018-02-22 18:12:39 +0100

    core config update

--- convertow/convertow-core/src/main/java/com/convertow/ConvertOwCore.java
+++ convertow/convertow-core/src/main/java/com/convertow/ConvertOwCore.java
@@ -8,5 +8,27 @@ package com.convertow;
  */
 public class ConvertOwCore {
     /* you can optionally implement info.magnolia.module.ModuleLifecycle */
+    public String path;
+    public String fileDelimiter;
 
+    public ConvertOwCore(){
+        path = getPath();
+        fileDelimiter = getFileDelimiter();
+    }
+
+    public String getFileDelimiter() {
+        return fileDelimiter;
+    }
+
+    public void setFileDelimiter(String fileDelimiter) {
+        this.fileDelimiter = fileDelimiter;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 }
